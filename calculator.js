7124a6c9d9f4972b0aa0b09540e5947621dc52dc
ash1ni <itsashwani001@gmail.com>
2022-07-28 20:38:34 +0530

    adding initial files

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.listen(3000, ()=>{
+    console.log("port listening on port 3000")
+})
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+ app.post("/", (req, res)=>{
+    var num1 = Number(req.body.num1)
+    var num2 = Number(req.body.num2)
+    var result = num1+num2
+
+
+    //console.log(req.body)
+    res.send("The result of the calculation is "+ result)
+ })
